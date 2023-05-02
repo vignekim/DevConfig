29cc3238bd2f371b603f8f00e3daf990b6529c7c
folder <shell@folderui-Macmini.local>
2023-05-02 13:17:02 +0900

    git config file delete

--- server/app1/src/main/java/com/folder/app/ApiController.java
+++ server/app1/src/main/java/com/folder/app/ApiController.java
@@ -1,5 +1,6 @@
 package com.folder.app;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,9 +10,15 @@ import lombok.extern.slf4j.Slf4j;
 @RestController
 public class ApiController {
 
+  @Value(value = "${user.message}")
+  private String message;
+
   @GetMapping("/")
   public String test() {
     log.info("App1 - Test()");
+
+    log.info("message : {}", message);
+
     return "App1 - Test()";
   }
 
